7f8c4c5ece0c75e82f5173174be182b27d3ae253
Paulbashk <101927296+Paulbashk@users.noreply.github.com>
2023-01-10 18:22:29 +0700

    update router

--- router/Router.php
+++ router/Router.php
@@ -1,288 +1,122 @@
 <?php
-	$url = $_SERVER['REQUEST_URI'];
-	$method = $_SERVER['REQUEST_METHOD'];
 
-	$urlArray = explode('?', $url);
-	$urlParam = $urlArray[1];
+	class Router {
+		// Список URL адресов
+		public $urls = Array();
 
-	$uri = $urlArray[0];
+		// Функция регистрирует обработчик
+		public function route($method, $url, $callback) {
+			// Записываем в URLS (ассоциативный массив):
+			// $url - id элемента
+			// method - метод отправки данных
+			// callback - функция
+			if (!$this->urls[$method]) {$this->urls[$method] = Array();};
+			$this->urls[$method][$url] = Array(
+				"callback" => $callback
+			);
+		}
 
-	// Маршрутизация
-	switch ($uri) {
-		case '/': {
-			switch($method) {
-				case 'POST': {
-					break;
-				}
-				case 'GET': {
-					require './view/build/index.php';				
-					break;
-				}				
+		// Функция обработки URL
+		public function processUrl() {
+			$url = explode("?", $_SERVER['REQUEST_URI'])[0]; // URL строка из адресной строки
+
+			// Метод передачи данных
+			$method = $_SERVER['REQUEST_METHOD'];
+
+			// Если url имеет в конце слеш, то удаляем его
+			if (mb_substr($url, -1, 1) == "/") { 
+				$url = mb_substr($url,0, -1);
 			}
-			break;
+
+			$urlAr = explode("/", $url); // Разбиваем URL на массив
+			$find = false; // Переменная поиска коллбека 
+
+			// Перебираем URL зарегистрированных коллбеков
+			
+			if ($this->urls[$method]) {
+				foreach ($this->urls[$method] as $rk => $rout) {
+					// $rk - URL коллбека, $rout - коллбек (Метод, функция)
+					$vars = Array(); // Очищаем переменные, чтоб данные не пропали с прошлых обработок
+					$ok = true; // Переменная проверки на поиск подходящего коллбека
+					$rurl = explode('/', $rk); // Разбиваем URL коллбека
+
+					// Если кол-во частей ссылки совпадает, то начинаем перебор
+					if(count($rurl) == count($urlAr)) {
+						// Перебираем URL коллбека по частям
+						foreach($rurl as $sk => $sub) {
+							// $sk - ключ части URL, $sub - текст части URL
+
+							// Если часть коллбека не является переменной
+							if(mb_substr($sub, 0, 1) !== "{") {
+
+								// Если переменная не совпадает
+								if($sub !== $urlAr[$sk]) {
+									// Прекращаем перебор коллбека
+									$ok = false;
+									break;
+								}
+							} else { // Если часть является переменной
+								// Если в URL нечем занменить переменную
+								if(!isset($urlAr[$sk])) {
+									// Прекращаем перебор коллбека
+									$ok = false;
+									break;			
+								}
+
+								// Если проблем никаких нет, объявляем переменную, записываем переменную
+								$vars[mb_substr($sub, 1, -1)] = $urlAr[$sk];
+							};
+						};
+
+						// Если всё нормально, проверяем метод, выполняем коллбек
+						if($ok) {
+							switch ($method) {
+								case 'GET':
+									$vars = array_merge($_GET,$vars);
+								break;
+								case 'POST':
+									$vars = array_merge($_POST,$vars);			
+								break;
+							};
+
+							$find = true; // Нашли подходящий коллбек
+								// Метод совпал, выполняем коллбек
+								$rout['callback']($vars);
+							}	
+						}
+					}
+				} else {
+					echo "Не найден метод";
+				};
+
+			if ($find == false) {
+				echo "Не найден подходящий обработчик";
+			};				
 		}
-		case '/register': {
-			switch($method) {
-				case 'POST': {
-					break;
-				}
-				case 'GET': {
-					require './view/build/index.php';			
-					break;
-				}				
-			}
-			break;
-		}	
-		case '/auth': {
-			switch($method) {
-				case 'POST': {
-					break;
-				}
-				case 'GET': {
-					require './view/build/index.php';					
-					break;
-				}				
-			}
-			break;
-		}	
-		case '/cabinet': {
-			switch($method) {
-				case 'POST': {
-					break;
-				}
-				case 'GET': {
-					require './view/build/index.php';					
-					break;
-				}				
-			}
-			break;
-		}
-		case '/cabinet/sale': {
-			switch($method) {
-				case 'POST': {
-					break;
-				}
-				case 'GET': {
-					require './view/build/index.php';					
-					break;
-				}				
-			}
-			break;
-		}
-		case '/cabinet/employee': {
-			switch($method) {
-				case 'POST': {
-					break;
-				}
-				case 'GET': {
-					require './view/build/index.php';					
-					break;
-				}				
-			}
-			break;
-		}
-		case '/cabinet/employee/add': {
-			switch($method) {
-				case 'POST': {
-					break;
-				}
-				case 'GET': {
-					require './view/build/index.php';					
-					break;
-				}				
-			}
-			break;
-		}
-		case '/cabinet/prices': {
-			switch($method) {
-				case 'POST': {
-					break;
-				}
-				case 'GET': {
-					require './view/build/index.php';					
-					break;
-				}				
-			}
-			break;
-		}
-		case '/cabinet/prices/add': {
-			switch($method) {
-				case 'POST': {
-					break;
-				}
-				case 'GET': {
-					require './view/build/index.php';					
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/register': {
-			switch($method) {
-				case 'POST': {
-					dataUserRegister($_POST);
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/auth': {
-			switch($method) {
-				case 'POST': {
-					dataUserAuth($_POST);
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/user': {
-			switch($method) {
-				case 'POST': {
-					isUserAuth();
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/user/sales': {
-			switch($method) {
-				case 'POST': {
-					getUserSales($_POST);
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/salesman/sales': {
-			switch($method) {
-				case 'POST': {
-					getDataSalesSalesman($_POST);
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/staff': {
-			switch($method) {
-				case 'POST': {
-					getAllStaff();
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/prices': {
-			switch($method) {
-				case 'POST': {
-					getAllPrices();
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/sales': {
-			switch($method) {
-				case 'POST': {
-					getAllSales();
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/category': {
-			switch($method) {
-				case 'POST': {
-					getAllCategory();
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/category/prices': {
-			switch($method) {
-				case 'POST': {
-					getPricesForCategory($_POST);
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}	
-		case '/api/users': {
-			switch($method) {
-				case 'POST': {
-					getAllUsers();
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/sale/add': {
-			switch($method) {
-				case 'POST': {
-					addSale($_POST);
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/sale/update': {
-			switch($method) {
-				case 'POST': {
-					updateSale($_POST);
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}
-		case '/api/sale/delete': {
-			switch($method) {
-				case 'POST': {
-					deleteSale($_POST);
-					break;
-				}
-				case 'GET': {				
-					break;
-				}				
-			}
-			break;
-		}																						
 	}
+
+	$Router = new Router;
+
+	/*
+	EXAMPLE
+
+	$Router->route("GET", "test", function() {
+		echo '123';
+	}););
+
+	$Router->route("DELETE", "test", function() {
+		echo $_SERVER['REQUEST_METHOD'];
+	});
+
+	$Router->route("PATCH", "test", function() {
+		echo $_SERVER['REQUEST_METHOD'];
+	});
+
+	$Router->route("GET","test/{code}/{code2}",function($vars) { // Функция
+		print_r($vars);
+	});
+
+	print_r();
+	$Router->processUrl();
+	*/
+
